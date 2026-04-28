f698579fbc65d2dac1d7cc17daa0d858a769b100
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-04-27 21:06:46 -0500

    Add files via upload

---
+++ Windows Firewall Script/Block_Opera.ps1
@@ -0,0 +1,35 @@
+﻿
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as Administrator."
+    return 
+}
+else {
+    Write-Host "Running as Administrator."
+}
+
+
+function Block_Opera{
+
+    $path = "$env:LOCALAPPDATA\Programs\Opera\launcher.exe"
+
+    if(-not(Test-Path $path)){
+        Write-Host "$path is not installed on machine..."
+        return
+    }
+    
+    $ruleName = "Block Opera"
+
+    if (Get-NetFirewallRule -DisplayName $ruleName -ErrorAction SilentlyContinue) {
+        Write-Host "Firewall rule '$ruleName' exists."
+    } else {
+        Try{
+            New-NetFirewallRule -DisplayName $ruleName -Direction Outbound -Program $path -Action Block
+        } Catch{
+             Write-Host "Failed to create $ruleName..."
+        }
+    }
+
+    Get-NetFirewallRule -DisplayName "Block Opera" | Format-List
+}
+
+Block_Opera
